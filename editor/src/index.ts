c518d728825da8f71b017d4f366f3807f50add78
Thomas <1779937+parsehex@users.noreply.github.com>
2019-07-29 07:16:50 -0400

    split editor css

--- editor/src/index.ts
+++ editor/src/index.ts
@@ -1,8 +1,13 @@
 import state from '../../src/state';
 import elements, { initElements } from '../../src/elements';
 import drawScene from '../../src/draw-scene';
+import { loadData } from '../../src/data';
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
+	// the game tries to load the normal js and 404s so clear that
+	console.clear();
+
+	await loadData();
 	state.currentDialogueIndex = 0;
 
 	// hook up the elements to the elements within the iframe
