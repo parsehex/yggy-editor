28be9b3985590dc4012b20f71d9514ca3aa8125c
Thomas <1779937+parsehex@users.noreply.github.com>
2019-08-02 09:59:40 -0400

    reopen last tab

--- editor/src/tabs.ts
+++ editor/src/tabs.ts
@@ -1,9 +1,18 @@
 import _editorDelegate from 'delegate';
 import updateActiveTab from 'update-tab';
+import { querySelector } from 'dom-util';
 
 const tabsListContainer = document.getElementById('tabs-list');
 
 export function initTabs() {
+	const lsTab = localStorage.getItem('editor-opened-tab');
+	if (lsTab) {
+		setTimeout(() => {
+			const tabButton = querySelector(`#tabs-list span.link[data-target="${lsTab}"]`);
+			if (tabButton) tabButton.click();
+		}, 0);
+	}
+
 	_editorDelegate('#tabs-list span.link', 'click', (e, t: HTMLButtonElement) => {
 		// deactivate last tab
 		document.querySelector('.tab.active').classList.remove('active');
@@ -13,6 +22,8 @@ export function initTabs() {
 		const tab = document.getElementById(tabId);
 		tab.classList.add('active');
 		t.classList.add('active');
+
+		localStorage.setItem('editor-opened-tab', t.dataset.target);
 
 		updateActiveTab();
 	});
