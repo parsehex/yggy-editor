dcce844820ea7332651952e947692d697196d37f
Thomas <1779937+parsehex@users.noreply.github.com>
2019-08-03 07:54:17 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,15 +8,20 @@ echo Stop editor server service
 sudo systemctl stop title-22-editor.service
 
 echo Clear trilium install directory
-cd ..
+cd $HOME
 rm -rf ./title-22
 
 echo Clone stable branch of trilium
-git clone -b stable https://github.com/parsehex/title-22.git
-chmod +x start-server.sh
+git clone -b master git@github.com:parsehex/title-22.git
+chmod +x ./start-server.sh
+chmod +x ./update.sh
+
+echo Updating systemd service file
+sudo cp ./systemd /etc/systemd/system/title-22-editor.service
+sudo systemctl daemon-reload
 
 echo npm install
-cd title-22
+cd ./title-22
 pnpm install
 
 echo Building game
