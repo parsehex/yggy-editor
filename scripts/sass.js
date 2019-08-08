c8d5015f11573bc7478c898a74ce226ad2b5ecce
Thomas <1779937+parsehex@users.noreply.github.com>
2019-08-08 14:08:51 -0400

    add parallel sass script

---
+++ scripts/sass.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const parallel = require('./parallel');
+
+const base = path.resolve(__dirname, '../');
+
+const cmd = process.argv[2];
+
+if (cmd !== 'build' && cmd !== 'watch') {
+	console.log('Pass "watch" or "build" as first argument');
+	process.exit(1);
+}
+
+const defaultEntries = [
+	'game',
+	'editor',
+];
+const commands = defaultEntries.map(makeCmd);
+
+if (process.argv.length > 3) {
+	commands.length = 0;
+
+	for (let i = 2; i < process.argv.length; i++) {
+		const script = process.argv[i].toLowerCase();
+
+		if (defaultEntries.indexOf(script) === -1) continue;
+
+		commands.push(makeCmd(script));
+	}
+}
+
+function makeCmd(s) {
+	const i = path.join(base, `${s}/scss/main.scss`);
+	const o = path.join(base, `${s}/assets/css/${s}.css`);
+	const opt = cmd === 'watch' ? '--watch' : '--style=compressed';
+	return {
+		name: s.toUpperCase(),
+		prefixColor: 'magenta',
+		command: `sass ${opt} ${i} ${o}`,
+	};
+}
+
+parallel(commands);
