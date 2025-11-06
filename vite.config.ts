3fc030b62cb058f616f216e4c09a2874f80f85a1
parsehex <1779937+parsehex@users.noreply.github.com>
2025-11-06 12:58:43 -0500

    install/setup tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 function serviceWorkerPlugin() {
 	return {
@@ -9,7 +10,10 @@ function serviceWorkerPlugin() {
 			server.middlewares.use(async (req, res, next) => {
 				if (req.url === '/service-worker.js') {
 					try {
-						const filePath = resolve(__dirname, './editor/service-worker/index.ts');
+						const filePath = resolve(
+							__dirname,
+							'./editor/service-worker/index.ts'
+						);
 						const result = await server.transformRequest(filePath);
 						if (result) {
 							res.setHeader('Content-Type', 'application/javascript');
@@ -32,7 +36,7 @@ export default defineConfig(({ mode }) => {
 	return {
 		root: resolve(__dirname, './editor'),
 		base,
-		plugins: [tsconfigPaths(), serviceWorkerPlugin()],
+		plugins: [tsconfigPaths(), serviceWorkerPlugin(), tailwindcss()],
 		define: {
 			__BASE__: JSON.stringify(base),
 		},
@@ -46,11 +50,16 @@ export default defineConfig(({ mode }) => {
 				input: {
 					game: resolve(__dirname, './editor/game/index.html'),
 					editor: resolve(__dirname, './editor/index.html'),
-					'service-worker': resolve(__dirname, './editor/service-worker/index.ts'),
+					'service-worker': resolve(
+						__dirname,
+						'./editor/service-worker/index.ts'
+					),
 				},
 				output: {
 					entryFileNames: (chunkInfo) =>
-						chunkInfo.name === 'service-worker' ? 'service-worker.js' : '[name].js',
+						chunkInfo.name === 'service-worker'
+							? 'service-worker.js'
+							: '[name].js',
 				},
 			},
 		},
